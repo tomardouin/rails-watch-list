bfa70b9cc151b515c48093750960abf66eebcec4
Tom Ardouin <tom.ardouin@icloud.com>
2024-07-05 12:02:58 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "movies", to: "movies#index"
-
   resources :lists, only: [:index, :new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
